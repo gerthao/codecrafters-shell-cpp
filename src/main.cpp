9b340b76d3cc4f0c4b2009d426107905ee29c282
Ger Thao <g.thao19@gmail.com>
2026-05-02 13:25:37 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,55 @@
 #include <iostream>
 #include <string>
 #include <print>
+#include <ranges>
+#include <vector>
+#include <istream>
+#include <sstream>
+
+void echo(std::ranges::input_range auto input) {
+    for (auto i = 0; i < input.size() - 1; ++i) {
+        std::print("{} ", input[i]);
+    }
+    std::print("{}", input.back());
+
+    std::println( "");
+}
+
+std::vector<std::string> parse_and_trim_input(const std::string &input) {
+    std::istringstream stream(input);
+    std::vector<std::string> tokens{
+        std::istream_iterator<std::string>{stream},
+        std::istream_iterator<std::string>{}
+    };
+    return tokens;
+}
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  while (true) {
-    std::print("$ ");
-    std::string input;
-    std::getline(std::cin, input);
+    while (true) {
+        std::print("$ ");
+        std::string input;
+        std::getline(std::cin, input);
 
-    if (input == "exit") {
-      return 0;
+        if (input == "exit") {
+            return 0;
+        }
+
+        auto tokens = parse_and_trim_input(input);
+        if (tokens.empty()) {
+            continue;
+        }
+
+        auto head = tokens.front();
+        auto tail = tokens | std::views::drop(1);
+
+        if (head == "echo") {
+            echo(tail);
+        } else {
+            std::println("{}: command not found", input);
+        }
     }
-
-    std::println("{}: command not found", input);
-  }
 }

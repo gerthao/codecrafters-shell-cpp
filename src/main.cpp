2b7b7c53699b52c43b6ab27f3172021a0c8c75ce
Ger Thao <g.thao19@gmail.com>
2026-05-02 13:03:34 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,20 @@
 #include <string>
 #include <print>
 
-[[noreturn]] int main() {
+int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
+
   while (true) {
     std::print("$ ");
     std::string input;
     std::getline(std::cin, input);
+
+    if (input == "exit") {
+      return 0;
+    }
+
     std::println("{}: command not found", input);
   }
 }
